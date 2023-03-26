4126d0799509841d41788c95359b02b59bdbf514
mitsui <mitsui@sobuline.com>
2023-03-26 22:16:51 +0900

    fix

--- vim/init.lua
+++ vim/init.lua
@@ -339,6 +339,9 @@ require("lazy").setup({
   },
   {
     'jlanzarotta/bufexplorer',
+    init = function()
+      vim.g.bufExplorerDisableDefaultKeyMapping = 1
+    end,
     keys = {
       { ',l', ':BufExplorer<CR>', { silent = true } },
     },
