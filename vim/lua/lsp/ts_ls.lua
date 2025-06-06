8c9dfb9e6a388e4f85514e93d65c5cdbc3784ff8
mitsui <mitsui@sobuline.com>
2025-06-07 05:27:19 +0900

    Fix: lsp setting

--- vim/lua/lsp/ts_ls.lua
+++
@@ -1,5 +0,0 @@
- return {
-	 root_dir = lspconfig.util.root_pattern('tsconfig.json', 'package.json'),
-	 single_file_support = true,
-	 root_markers = {'package.json'}:help lspconfig-all,
- }
--- vim/lua/plugins/mason-lspconfig.lua
+++ vim/lua/plugins/mason-lspconfig.lua
@@ -1,10 +1,16 @@
 local config = function()
   require('mason-lspconfig').setup({
-    automatic_installation = {
-      exclude = {
-        'gopls',
-      }
-    }
+    ensure_installed = {
+      'jsonls',
+      'rust_analyzer',
+      'ts_ls',
+      'yamlls',
+      'lua_ls',
+      'denols',
+      'bashls',
+      'marksman',
+      'intelephense'
+    },
   })
 end
 
@@ -15,9 +21,34 @@ local mason = {
       'williamboman/mason.nvim',
       config = function()
         require('mason').setup()
-        require('mason-lspconfig').setup()
       end,
     },
+    {
+      'neovim/nvim-lspconfig',
+      config = function()
+        vim.diagnostic.config({ virtual_text = true })
+        local augroup = vim.api.nvim_create_augroup('lsp/init.lua', {})
+        vim.api.nvim_create_autocmd('LspAttach', {
+          group = augroup,
+          callback = function(args)
+            local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+          end,
+        })
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.completion.completionItem.resolveSupport = {
+          properties = {
+            "documentation",
+            "detail",
+            "additionalTextEdits",
+          },
+        }
+        vim.lsp.config("*", { capabilities = capabilities })
+      end,
+      event = { "BufReadPre", "BufNewFile" },
+    }
   },
   config = config,
   event = { 'BufReadPre', 'BufNewFile', 'BufEnter', 'BufNew' },
