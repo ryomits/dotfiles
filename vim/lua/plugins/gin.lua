ff4303049c4ee758cb17eb8b7bffac40b0e1c41c
mitsui <mitsui@sobuline.com>
2025-06-08 20:35:14 +0900

    Fix: browse keybinding in gin plugin

--- vim/lua/plugins/gin.lua
+++ vim/lua/plugins/gin.lua
@@ -1,7 +1,7 @@
 local config = function()
   vim.keymap.set('n', 'gs', '<Cmd>GinStatus<CR>')
   vim.keymap.set('n', 'gl', '<Cmd>GinLog -p<CR>')
-  vim.keymap.set('n', 'ob', '<Cmd>GinBrowse<CR>')
+  vim.keymap.set('n', 'gh', '<Cmd>GinBrowse<CR>')
 end
 
 local gina = {
