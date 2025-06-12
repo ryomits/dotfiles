0b137cfd49f234a308cc0bf799ecb9afe0f693ad
mitsui <mitsui@sobuline.com>
2025-06-12 22:31:17 +0900

    feat(vim-test): update keymap and plugin configuration
    Updated indentation for keymap and plugin configuration in test.lua
    to improve readability and maintain consistency across the file.

--- vim/lua/plugins/test.lua
+++ vim/lua/plugins/test.lua
@@ -1,12 +1,12 @@
 local config = function()
-	vim.keymap.set('n', '<Leader>tt', '<Cmd>TestFile<CR>')
-	vim.keymap.set('n', '<Leader>tn', '<Cmd>TestFile<CR>')
+  vim.keymap.set('n', '<Leader>tt', '<Cmd>TestFile<CR>')
+  vim.keymap.set('n', '<Leader>tn', '<Cmd>TestFile<CR>')
 end
 
 local test = {
-	'vim-test/vim-test',
-	event = 'BufRead',
-	config = config,
+  'vim-test/vim-test',
+  event = 'BufRead',
+  config = config,
 }
 
 return test
