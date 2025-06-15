18304b7f91bf8de8e8898297262b5f0ac2c343a9
mitsui <mitsui@sobuline.com>
2025-06-15 16:22:19 +0900

    feat(vim): remove unused templates and update setup script
    The following changes were made:
    - Deleted unused templates for Go, Markdown, and TypeScript.
    - Removed template linking logic from the setup script.
    
    This cleanup reduces clutter and simplifies the project structure.

--- vim/setup.sh
+++ vim/setup.sh
@@ -26,7 +26,3 @@ fi
 if [[ ! -e $HOME/.vimrc ]]; then
   ln -s "$PWD"/.vimrc "$HOME"/.vimrc
 fi
-
-if [[ ! -e $path/template ]]; then
-  ln -s "$PWD"/template $path/template
-fi
--- vim/template/markdown/base-article.md
+++
@@ -1,7 +0,0 @@
----
-title: {{_input_:title}}
-date: {{_expr_:strftime('%Y-%m-%d')}}
-description: {{_input_:title}}
----
-
-{{_cursor_}}
